b8e584257c84e915a4956925f1a6351f5c4f27e4
dkkwong <davidkwong6@gmail.com>
2022-05-02 14:31:31 -0500

    Add satellite basemap
    Add layer control

--- webmapping_final_group-main/js/main.js
+++ webmapping_final_group-main/js/main.js
@@ -1,19 +1,49 @@
 var map;
 //function to instantiate the Leaflet map
 function createMap(){
-    //create the map
-    map = L.map('map', {
-        center: [20, 0],
-        zoom: 2
-    });
+    
     //tileset from leaflet-extras.github.io/leaflet-providers/preview/
+    //base layers
     var Stamen_Watercolor = L.tileLayer('https://stamen-tiles-{s}.a.ssl.fastly.net/watercolor/{z}/{x}/{y}.{ext}', {
         attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
         subdomains: 'abcd',
         minZoom: 1,
         maxZoom: 16,
         ext: 'jpg'
-    }).addTo(map);
+    })
+    var Esri_WorldImagery = L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}', {
+	attribution: 'Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community'
+    })
+    
+    //overlay layer
+    var Stamen_TonerLabels = L.tileLayer('https://stamen-tiles-{s}.a.ssl.fastly.net/toner-labels/{z}/{x}/{y}{r}.{ext}', {
+        attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+        subdomains: 'abcd',
+        minZoom: 0,
+        maxZoom: 20,
+        ext: 'png'
+    })
+   
+    //create the map
+    map = L.map('map', {
+        center: [20, 0],
+        zoom: 2,
+        layers: [Stamen_Watercolor,Stamen_TonerLabels] //watercolor is default base layer with labels as overlay
+    });
+    var baseMaps = {
+        "Watercolor": Stamen_Watercolor,
+        "Satellite": Esri_WorldImagery
+    };
+    
+    var overlayMaps = {
+        "Labels": Stamen_TonerLabels
+    };
+
+    //add layer control to the map
+    L.control.layers(baseMaps, overlayMaps).addTo(map);
+
+
+
     //call data function
     getData()
     
